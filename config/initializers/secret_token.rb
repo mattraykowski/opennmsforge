412d62820fa598da8fee9a10705a6e8d769e116a
Matt Raykowski <matt.raykowski@gmail.com>
2013-06-26 12:18:54 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opennmsforge::Application.config.secret_token = '5ee1c65af6c57b9bb0ec442235b4c67e6ded0d575d031093b081eb8c0b1f8b80f26173b1623049c8feca0711fccfd2f70f1a23591b1d629299638d20b89b0897'
