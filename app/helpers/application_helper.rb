4b48c62b2be2aea958437c621323deefa8de70f5
Matt Raykowski <matt.raykowski@gmail.com>
2013-12-11 15:07:50 +0000

    More bootstrap 3 conversion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,23 @@
 module ApplicationHelper
   def flash_class(level)
     case level
-    when :notice then "info"
-    when :error then "error"
-    when :alert then "warning"
+    when :notice then "alert-success"
+    when :error then "alert-danger"
+    when :alert then "alert-warning"
     end
   end
+      
+  def field_status(resource, field)
+    'has-error' if resource.errors.has_key? field
+  end
+      
+  def field_status_message(resource, field)
+    if resource.errors.has_key? field
+      status_message = resource.errors.full_message(field, resource.errors.get(field)[0])
+      raw('<span class="help-block">'+status_message+'</span>')
+    else
+      ''
+    end
+  end
+        
 end
